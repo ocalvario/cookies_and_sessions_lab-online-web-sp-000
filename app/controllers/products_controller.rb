5a82157df83b71e72d495cebf99b210de32a5406
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-22 18:14:16 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,16 +5,12 @@ class ProductsController < ApplicationController
 
 # Routed from POST /items/:id/add_to_cart
    def add
-	# Step One: Get the item from the path
-
-  # Step Two: Then load the cart from the session using
-  # our helper method in application controller
 
      @product = Product.find(params[:id])
   	 cart<< @product.id
-   #or could do it all in one step, using params hash
+   
     cart << params[:product]
-    #Step 3: render the view from index
+  
    	render :index
 	end
 
